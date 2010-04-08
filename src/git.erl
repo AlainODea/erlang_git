3ed3cdc6e5341091852418992626a3d10a4ded91
Alain O'Dea <alain.odea@gmail.com>
2010-04-08 08:16:32 -0230

    Got parsing to tree records working

--- src/git.erl
+++ src/git.erl
@@ -6,11 +6,8 @@ show_head() -> show_ref(file:read_file(".git/HEAD")).
 
 show_ref({ok, RefOrSHA}) -> show_sha(sha(RefOrSHA)).
 
-show_sha(<<CommitSHAHead:2/binary, CommitSHATail:38/binary, "\n">>) ->
-    show_commit(binary_to_list(CommitSHAHead), binary_to_list(CommitSHATail)).
-
-show_commit(CommitSHAHead, CommitSHATail) ->
-    inflate_file([".git/objects", "/" ++ CommitSHAHead, "/" ++ CommitSHATail]).
+show_sha(<<SHAHead:2/binary, SHATail:38/binary>>) ->
+    inflate_file([".git/objects", "/" ++ binary_to_list(SHAHead), "/" ++ binary_to_list(SHATail)]).
 
 inflate_file(File) ->
     {ok, Binary} = file:read_file(File),
@@ -23,12 +20,12 @@ inflate(Binary) ->
 
 sha(<<"ref: ", Ref/binary>>) ->
     {ok, SHA} = file:read_file(filename(Ref)),
-    SHA;
+    drop_last(SHA);
 sha(<<CommitSHA/binary>>) -> drop_last(CommitSHA).
 
-filename(<<Ref/binary>>) -> [".git/", drop_last(Ref)].
+filename(<<Ref/binary>>) -> [".git/", binary_to_list(drop_last(Ref))].
 
 drop_last(String) ->
 	Size = byte_size(String) - 1,
 	<<Trimmed:Size/bytes, "\n">> = String,
-	binary_to_list(Trimmed).
+	Trimmed.
--- src/git_tree.erl
+++ src/git_tree.erl
@@ -4,11 +4,9 @@
 
 -export([parse/1]).
 
-parse(#object{type = tree, data = Rest}) ->
-	{Entries, _Rest} = parse_entries(Rest), 
-	#tree{entries = Entries}.
+parse(#object{type = tree, data = Rest}) -> #tree{entries = entries(Rest)}.
 
-parse_entries(<<Mode:6/bytes, " ", PathSHA1Rest/binary>>) ->
+entries(<<Mode:6/bytes, " ", PathSHA1Rest/binary>>) ->
     {Path, <<SHA1:160/bits, Rest/bits>>} = git_string:null_split(PathSHA1Rest),
-    [#tree_entry{mode=Mode, path=Path, sha1=SHA1}| parse_entries(Rest)];
-parse_entries(<<_Rest/binary>>) -> [].
+    [#tree_entry{mode=Mode, path=Path, sha1=SHA1}| entries(Rest)];
+entries(<<_Rest/binary>>) -> [].
